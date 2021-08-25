ee93595e743c3ce1b8e79091ada21ede2ef0a627
Yar0s1av <yros1av@yandex.ru>
2021-08-25 18:30:10 +0600

    Затычка от вылета при отсутствии звуковой карты или при ключе -nosound

--- editors/ShaderEditor/SHSoundEnvTools.cpp
+++ editors/ShaderEditor/SHSoundEnvTools.cpp
@@ -63,8 +63,17 @@ void CSHSoundEnvTools::OnChangeWAV	(PropValue* prop)
 	if (m_SoundName.size()){
     	m_PreviewSnd.create				(*m_SoundName,st_Effect,sg_Undefined);
         CSoundRender_Source* src= (CSoundRender_Source*)m_PreviewSnd._handle();
-        m_Params.min_distance	= src->m_fMinDist;
-        m_Params.max_distance	= src->m_fMaxDist;
+
+		if(src)
+		{
+			m_Params.min_distance	= src->m_fMinDist;
+			m_Params.max_distance	= src->m_fMaxDist;
+		}
+		else
+		{
+			m_Params.min_distance = 1.f;
+			m_Params.max_distance = 10.f;
+		}
     }
 	if (bPlay) 		m_PreviewSnd.play	(0,sm_Looped);
 }
